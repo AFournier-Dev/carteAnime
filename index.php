e38629efe98b134c4926a61bb021cf6ba4fef742
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-12-14 13:25:50 +0100

    mise en place de la fonction mail

--- index.php
+++ index.php
@@ -31,6 +31,55 @@ require_once 'header.php';
 </div>
 
 
+<?php
+$to = 'adresse-email@fai.fr';
+
+if (preg_match("#@(hotmail|live|msn).[a-z]{2,4}$#", $to))
+{
+    $passage_ligne = "\n";
+}
+else
+{
+    $passage_ligne = "\r\n";
+}
+
+$boundary = "-----=".md5(rand());
+
+$sujet = "Meilleurs voeux";
+$message_html = '.....';
+$message_txt = strip_tags($message_html);
+
+$headers = "From: \"Nom\"<adresse@fai.fr>" . $passage_ligne;
+$headers.= "Reply-to: \"Nom\" <adresse@fai.fr>" . $passage_ligne;
+$headers.= "MIME-Version: 1.0" . $passage_ligne;
+$headers.= "Content-Type: multipart/alternative;" . $passage_ligne . " boundary=\"" . $boundary . "\"" . $passage_ligne;
+
+$message = $passage_ligne . $boundary . $passage_ligne;
+
+$message .= "Content-Type: text/plain; charset=\"ISO-8859-1\"" . $passage_ligne;
+$message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
+$message .= $passage_ligne . $message_txt . $passage_ligne;
+
+$message .= $passage_ligne . "--" . $boundary . $passage_ligne;
+
+$message .= "Content-Type: text/html; charset=\"ISO-8859-1\"" . $passage_ligne;
+$message .= "Content-Transfer-Encoding: 8bit" . $passage_ligne;
+$message .= $passage_ligne . $message_html . $passage_ligne;
+
+$message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+$message .= $passage_ligne . "--" . $boundary . "--" . $passage_ligne;
+
+if (mail($to, $sujet, $message, $headers))
+{
+    echo 'E-Mail envoyé !';
+}
+else
+{
+    echo "Impossible d'envoyer le E-Mail";
+}
+?>
+
+
 
 
 

03bdedc6112129e9a91cf6f99823642fa12b0798
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-12-16 11:19:16 +0100

    template intégrer dans le indexcopy TBD padding formulaire & fusion inexcopy en index WIP

--- emailtemplate.php
+++ emailtemplate.php
@@ -1,12 +1,24 @@
+<?php
+$firstChar = strtoupper(mb_substr(trim(htmlspecialchars($_POST['nom'])), 0, 1, 'UTF-8'));
+$letters =['A', 'E', 'I', 'O', 'U', 'H', 'Y'];
+if (in_array($firstChar, $letters)){
+    $text = "d' ". htmlspecialchars($_POST['nom']);
+}
+else{
+    $text = "de ". htmlspecialchars($_POST['nom']);
+}
+$message = '
+
 <!DOCTYPE html
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns:v="urn:schemas-microsoft-com:vml">
 
 <head>
+    <title>Carte de voeux</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
     <!--ICI POSSIBILITE DE META AVEC GOOGLE FONT SUPORTER PAR CERTAIN CLIENTS MAIS NE CASSE PAS LE CODE SI NON SUPPORTE-->
-    <link href='https://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet" type="text/css">
 </head>
 
 <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
@@ -30,7 +42,7 @@
                                     cellspacing="0">
                                     <tbody>
                                         <tr>
-                                            <td height="250" style="font:size: 30px; line-height: 30px;">
+                                            <td height="250" style="font-size: 30px; line-height: 30px;">
                                                 &nbsp;
                                             </td>
                                         </tr>
@@ -44,21 +56,20 @@
                                         <tr>
                                             <td align="center"
                                                 style="font-family: Arial, Helvetica, sans-serif; text-align: center; font-size: 48px; color:#ffffff; mso-line-height-rule: exactly; line-height: 58px;">
-                                                Je vous ai envoyer une carte de voeux
+                                                Vous avez reçu une carte de voeux
                                             </td>
                                         </tr>
                                         <tr>
-                                            <td height="250" style="font:size: 30px; line-height: 30px;">
+                                            <td height="250" style="font-size: 30px; line-height: 30px;">
                                                 &nbsp;
-                                                <a
-                                                    href='https://alexandref544.promo-45.codeur.online/cartedevoeux/card.html'>Vous
-                                                    avez recu une carte de la part de
-                                                    ".htmlspecialchars($_POST['nom'])."</a>
+                                                <a href="https://alexandref544.promo-45.codeur.online/cartedevoeux/card.html">Vous
+                                                    avez recu une carte de la part '.
+                                                    $text.'</a>
                                             </td>
                                             
                                         </tr>
                                         <tr>
-                                            <td height="300" style="font:size: 30px; line-height: 30px;">
+                                            <td height="300" style="font-size: 30px; line-height: 30px;">
                                                 &nbsp;
                                             </td>
                                         </tr>
@@ -84,3 +95,5 @@
 
 
 </html>
+
+';

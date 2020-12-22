a5961825219299f3d32dedf641d41c661d004eae
AFournier-Dev <alexandre.fournier@yahoo.fr>
2020-12-22 13:42:48 +0100

    OK V4

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,12 +11,11 @@ const svg = document.querySelector('svg');
 function sensDeLecture () {
  //   if(window.orientation === 90 || window.orientation === -90){
      if((window.orientation >=75 && window.orientation<=105) || (window.orientation <=-75 && window.orientation>=-105)){
+        svg.setAttribute("viewBox", "352.5 0 474.5 807");
+        svg.setAttribute("enable-background", "new 352.5 0 474.5 807");
+    }else{
         svg.setAttribute("viewBox", "0 0 1280 807");
         svg.setAttribute("enable-background", "new 0 0 1280 807");
-    }else{
-        svg.setAttribute("viewBox", "352.5 0 474.5 807");
-        svg.setAttribute("enable-background", "new 352.5 0 474.5 807"
-        );
     }
 }
 sensDeLecture();
